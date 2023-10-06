784cb13eaca7e20ff59524c9368a225cd6c06bf4
girum <girumassefa6@gmail.com>
2023-10-06 18:43:04 -0400

    c for loop alphabet project 2 corrected

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the program main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
-
 /**
  * main - a program that prints alphabet
  *
  * Return: always 0
  */
-int main(void)
-{
-	char letter;
 
-	for (letter = 'a'; letter  <= 'z'; letter++)
-		putchar(letter);
-	putchar('\n');
-	return (0);
-}
+int main(void)
+  {
+	   printf("a-z A-Z\n");
+		  for (char i = 'a'; i <= 'z'; i++)
+		       	{
+				        printf("%c", i);
+					    }
+			    for (char i = 'A'; i <= 'Z'; i++)
+			    {
+				            printf("%c", i);
+					        }
+			        printf("\n");
+
+				    return (0);
+	}
+
