cc5256b3c724f42043733fd8985298c10a0b75ac
girum <girumassefa6@gmail.com>
2023-10-07 07:19:45 -0400

    this is my random number to the variable project 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,30 +5,36 @@
 /* betty style doc for function main goes there */
 /**
  * main - main function
- *
+ * description: chechs the last digit
  * Return: always 0
  */
 
 int main(void)
 {
 		int n;
-		int m;
+
+		int s;
 
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
 					/* your code goes there */
-				m = n % 10;
-				if (m > 5)
+				s = n % 10;
+				if (s > 5)
+
 				{
-					printf("last digit of %d is %d and is greater than 5\n", n, m);
+					printf("Last digit of %d is %d and is greater than 5\n", n, s);
 				}
-				else if (m < 6 && m != 0)
+
+				else if (s < 6 && s != 0)
+
 				{
-					printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+					printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
 				}
+
 				else
+
 				{
-					printf("Last digit of %d is %d and is 0\n", n, m);
+					printf("Last digit of %d is %d and is 0\n", n, s);
 				}
 
 					return (0);
