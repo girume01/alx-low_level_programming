03a32252c584b93d28fbba9793c2df75f22f61b9
girum <girumassefa6@gmail.com>
2023-10-11 10:25:57 -0400

    corrected second test for print to page 98 project 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,39 +8,16 @@
  * Return - return 0
  *
  */
-
 void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)
-		{
-			if (n == 98)
-			{
-				printf("%d ", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
 	}
 	else
-	{
-		for (; n >= 98; n--)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
-	}
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+	printf("98");
+	printf("\n");
 }
