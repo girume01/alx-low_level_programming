72fa7237c71ef9f3c7fa8c83c0d269d148c45069
girum <girumassefa6@gmail.com>
2023-10-11 08:43:08 -0400

    this is the commit for 11-print_to_98.c project

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 0  && n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d ", i);
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+		printf("%d", n);
+	printf("\n");
+}
