d059aecff259753ebc5fbc35fbcf0ad1b8110347
girum <girumassefa6@gmail.com>
2023-10-11 06:41:08 -0400

    add two integers and returns the result project 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ *
+ * @j: integer to be added
+ *
+ * @i: integer to be add
+ * Return: always 0
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
