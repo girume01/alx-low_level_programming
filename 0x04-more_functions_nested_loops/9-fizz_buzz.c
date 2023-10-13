7b3621930f9a70ca90c295fdaf6df70c9faef25b
girum <girumassefa6@gmail.com>
2023-10-13 12:06:27 -0400

    this is fizzbuzz programming task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entering for fizzbuzz program
+ *
+ * Return: always zero
+ *
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+			printf("%s ", "FizzBuzz");
+		else if (x % 3 == 0)
+			printf("%s ", "Fizz");
+		else if (x % 5 == 0)
+			printf("%s ", "Buzz");
+		else
+			printf("%d ", x);
+	}
+	printf("\n");
+
+	return (0);
+}
