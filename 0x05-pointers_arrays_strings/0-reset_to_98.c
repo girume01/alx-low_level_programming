7997f7ce17375f23315a7b4513ca90df7bcee738
girum <girumassefa6@gmail.com>
2023-10-16 08:32:09 -0400

    0x05 pointer, array and string project 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - update the integer pointrd to by pointer 98
+ *
+ * @n: a pointer integer
+ *
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is readme for 0x05 c - pointers arrays and string projects
