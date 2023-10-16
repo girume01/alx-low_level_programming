dc6ada008acbefff9ff82b83bcbd4f6c6d71b418
girum <girumassefa6@gmail.com>
2023-10-16 18:06:55 -0400

    0x05 pointer array and string project 09

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - copies string 
+ * @dest: destination
+ * @src:source string
+ * Return: pointer to dust
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *start = dest;
+
+	for (; *src != '\0'; src++, dest++)
+	{
+		*dest = *src;
+	}
+	*dest = '\0';
+	return (start);
+}
