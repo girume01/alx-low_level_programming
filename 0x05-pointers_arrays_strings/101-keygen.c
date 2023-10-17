53ff7e421ed60c4c647af011de31e8cdf94e0a36
girum <girumassefa6@gmail.com>
2023-10-17 06:04:36 -0400

    0x05 pointer arrays and strings final project 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - program that generate random
+ * password for the program crackme
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int pass[100];
+	int i = 0;
+	int sum = 0;
+
+	srand(time(NULL));
+
+	while (i < 100)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 -sum) - '0' < 78)
+		{
+			int n = 2772 - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+		i++;
+	}
+	return (0);
+}
