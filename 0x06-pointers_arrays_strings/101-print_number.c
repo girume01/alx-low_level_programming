8c6a976c04e96d27054b9cdaa6e92d50609647af
girum <girumassefa6@gmail.com>
2023-10-19 14:42:36 -0400

    0x06 pointer array and string project 09

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	unsigned int y = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		y = -y;
+	}
+	if (y / 10)
+	{
+		print_number(y / 10);
+	}
+	_putchar(y % 10 + '0');
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,3 +10,4 @@ char *cap_string(char *);
 char *leet(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *rot13(char *);
+void print_number(int n);
