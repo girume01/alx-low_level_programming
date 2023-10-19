90bbb6f8ce5cd15ab92e7b85e55316f7cee4c2be
girum <girumassefa6@gmail.com>
2023-10-19 04:33:47 -0400

    0x06 pointer array and string final project 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * _strncat - concatenate two strings
+ * @dest: destination of string
+ * @src: source of string to be oppened
+ * @n: max number of characters
+ * Return: dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
 	int len = strlen(dest);
