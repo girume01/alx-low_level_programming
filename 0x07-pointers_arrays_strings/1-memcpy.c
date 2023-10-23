cf3083af73f0b0d86239d55665d3db287d9e492e
girum <girumassefa6@gmail.com>
2023-10-23 07:47:47 -0400

    0x07-pointers_arrays_strings project 01

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy - copy memory place
+ * @dest: destination of memory
+ * @src: source of memory
+ * @n: number of bytes
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int y;
+
+	for (y = 0; y < n; y++)
+	{
+		dest[y] = src[y];
+	}
+	return (dest);
+}
