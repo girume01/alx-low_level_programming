7b4850b3c53adc15d85a66618af3f5eb6b5ec6aa
girum <girumassefa6@gmail.com>
2023-10-28 05:25:06 -0400

    0x0A-argc_argv project 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point of program.
+ * @argc: number of command line argumment.
+ * @argv: an array of command line argument.
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
