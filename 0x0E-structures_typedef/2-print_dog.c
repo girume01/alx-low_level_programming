432ea9269d7141eb138e5223a3ee7d6fdfeec2ee
girum <girumassefa6@gmail.com>
2023-11-07 05:31:59 -0800

    0x0E structures typedef project 02

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints struct dog.
+ * @d: pointer to struct dog.
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+		printf("Age: %1f\n", d->age);
+		printf("Owner: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	}
+}
