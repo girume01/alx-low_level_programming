267b0b02cfc0f170fdac39c279fa5655a08df5f7
girum <girumassefa6@gmail.com>
2023-11-17 05:37:17 -0800

    0x12 singly linked list project 05

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * out_message - output text message.
+ */
+void out_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
