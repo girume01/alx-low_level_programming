4b465241089651c2bdebe00018777dd8eff9c289
girum <girumassefa6@gmail.com>
2023-11-30 10:27:13 -0800

    0x15 file io task 03

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,58 +1,83 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include "main.h"
 /**
- * errorf - checks if the file can be opened.
+ * errf - checks if the file can be opened.
  * @ff: file from.
  * @ft: file to.
  * @argv: argument vector.
  * Return: nothing will return.
  */
-void errorf(int ff, int ft, char *argv[])
+void errf(int ff, int ft, char *argv[])
 {
 	if (ff == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error %s\n", argv[1]);
+		{
+			exit(98);
+		}
 	}
-	if  (ft == -1)
+	if (ft == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error %s\n", argv[2]);
+		{
+			exit(99);
+		}
 	}
 }
 /**
  * main - entry of program.
- * @argc: number of argument.
- * @argv: argument vector.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
  * Return: 0.
  */
 int main(int argc, char *argv[])
 {
-	int ff, ft, errc;
-	ssize_t n, nwr;
-	char buf[1024];
+	int errc;
+	int ff, ft;
+	ssize_t newchars;
+	ssize_t newr;
+	char buff[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "%s\n", "Usage ");
+		{
+			exit(97);
+		}
 	}
 	ff = open(argv[1], O_RDONLY);
 	ft = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	errorf(ff, ft, argv);
+	errf(ff, ft, argv);
 
-	while ((n = read(ff, buf, 1024)) == 1024)
+	newchars = 1024;
+	while (newchars == 1024)
 	{
-		if (n == -1)
-			errorf(-1, 0, argv);
-		if ((nwr = write(ft, buf, n)) == -1)
-			errorf(0, -1, argv);
+		newchars = read(ff, buff, 1024);
+		if (newchars == -1)
+			errf(-1, 0, argv);
+		newr = write(ft, buff, newchars);
+		if (newr == -1)
+			errf(0, -1, argv);
 	}
-	if ((errc = close(ff)) == -1 || (errc = close(ft)) == -1)
+
+	errc = close(ff);
+	if (errc == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", errc);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error %d\n", ff);
+		{
+			exit(100);
+		}
+	}
+
+	errc = close(ft);
+	if (errc == -1)
+	{
+		dprintf(STDERR_FILENO, "Error %d\n", ff);
+		{
+			exit(100);
+		}
 	}
 	return (0);
 }
