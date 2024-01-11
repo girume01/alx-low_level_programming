bb68e50221e79a4a70795717d5f936568c66115a
girum <girumassefa6@gmail.com>
2024-01-11 05:33:09 -0800

    0x17 c task 3

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - free list
+ * @head: struct
+ *
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	while (head)
+	{
+		dlistint_t *temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
