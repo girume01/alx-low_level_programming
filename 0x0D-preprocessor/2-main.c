05f2c7d8737fe2da7a4d7bbbf3095b3b89639db6
girum <girumassefa6@gmail.com>
2024-04-03 01:06:41 -0700

    0x0D task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,14 @@
+#include "1-pi.h"
 #include <stdio.h>
+
 /**
- * main - prints the name of the file.
- * Return: 0.
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-int main()
+
+int main(void)
 {
-	printf("%s\n", __FILE__);
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
